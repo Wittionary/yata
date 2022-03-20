d9f66e75988bef8fb92cc4b46158a738030a4a60
Witt Allen <wittionary@users.noreply.github.com>
2022-03-19 22:34:55 -0500

    It's working!

--- levels.py
+++ levels.py
@@ -1,7 +1,18 @@
-import json
+# MVP
+# - checks to see that item(s) have been completed
+# - add points to experience system
+# - reflect XP increase in frontend
+
+#from genericpath import exists
+import json, time
+from telnetlib import theNULL
 import requests as r
 from todoist_api_python.api import TodoistAPI
 from datetime import datetime
+from os import path
+
+TODOIST_STORAGE_FILENAME = "todoist-storage.json"
+APP_STORAGE_FILENAME = "levels-storage.json"
 
 with open("config.json", "r") as f:
     config = json.load(f)
@@ -24,7 +35,7 @@ def initial_sync():
     payload = {"sync_token":"*", "resource_types":'["projects"]'}
     response = r.post(f"{endpoint_url}{path}", headers=headers, data=payload)
     response = json.loads(response.content)
-    with open("todoist-storage.json", "w") as f:
+    with open(TODOIST_STORAGE_FILENAME, "w") as f:
         f.write(str(response))
 
     sync_token = response["sync_token"]
@@ -34,13 +45,14 @@ def get_completed_tasks(since="2007-4-29T10:13"):
     """
     Get all completed tasks
     """
+    print("Pulling completed tasks from Todoist API")
     path = "/completed/get_all"
     payload = {"since":f"{since}"}
     response = r.post(f"{endpoint_url}{path}", headers=headers, data=payload)
     response = json.loads(response.content)
 
-    with open("todoist-storage.json", "w") as f:
-        f.write(str(response))
+    with open(TODOIST_STORAGE_FILENAME, "w") as f:
+        f.write(str(json.dumps(response)))
 
     last_sync = get_now()
     set_levels_storage("last_sync", last_sync)
@@ -58,15 +70,22 @@ def get_now():
     return now
 
 def set_levels_storage(key, value):
-    # open file
-    with open("levels-storage.json", "r") as f:
-        levels_storage = json.load(f)
+    # Create the storage file if it's not there
+    file_exists = path.exists(APP_STORAGE_FILENAME)
+    if not file_exists:
+        with open(APP_STORAGE_FILENAME, "w") as f:
+            print(f"Creating {APP_STORAGE_FILENAME}")
+        levels_storage = {}
+    else:
+        # open file
+        with open(APP_STORAGE_FILENAME, "r") as f:
+            levels_storage = json.load(f)
 
     # Add key-value pair to storage object
     levels_storage[key] = value
 
-    with open("levels-storage.json", "w") as f:
-        f.write(str(levels_storage))
+    with open(APP_STORAGE_FILENAME, "w") as f:
+        f.write(str(json.dumps(levels_storage)))
 
     return
 
@@ -74,24 +93,49 @@ def get_levels_storage(key):
     """
     Return the value of an object in app storage
     """
-    with open("levels-storage.json", "r") as f:
+    with open(APP_STORAGE_FILENAME, "r") as f:
         levels_storage = json.load(f)
 
     value = levels_storage[key]    
     
     return value
 
+def get_todoist_storage(key):
+    """
+    Return the value of an object in app storage
+    """
+    with open(TODOIST_STORAGE_FILENAME, "r") as f:
+        todoist_storage = json.load(f)
+
+    value = todoist_storage[key]
+    #print(f"{value}")    
+    
+    return value
+
+def completed_tasks_count():
+    count = len(get_todoist_storage("items"))
+    return count
+
+def increment_experience_points():
+    # add some numbers to levels storage
+    return
 
 # App logic -------------------------------------------------
 
 #sync_token = initial_sync()
+# DONE: Store the last_sync so we can check to see how far back we should look for newly/recently completed tasks
+# DONE: That probably belongs at the end of the get_completed_tasks() func
+
 get_completed_tasks()
-# Store the last_sync so we can check to see how far back we shoud look for newly/recently completed tasks
-# That probably belongs at the end of the get_completed_tasks() func
 
-# MVP
-# - checks to see when item(s) have been completed
-# - add points to experience system
-# - reflect XP increase in frontend
+#time.sleep(10)
+num_completed = completed_tasks_count()
+print(f"Num completed is: {num_completed}")
+print("Sleeping 20 seconds")
+time.sleep(20)
+get_completed_tasks(get_levels_storage("last_sync"))
+new_num_completed = completed_tasks_count()
+print(f"New num completed is: {new_num_completed}")
 
-# Test stuff with a sample project
+if new_num_completed > num_completed:
+    increment_experience_points()
